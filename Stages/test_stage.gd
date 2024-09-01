5114be8c38f65adb76cbdf9ae8d15b42f714aa92
Jason Swartz <jasonrswartz@gmail.com>
2024-08-31 20:08:19 -0600

    player cards

--- Stages/test_stage.gd
+++ Stages/test_stage.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 @export var p1: PackedScene
 @export var p2: PackedScene
+@onready var p1portrait = $CanvasLayer/Control/HBoxContainer/P1Card/HBoxContainer/Portrait
+@onready var p2portrait = $CanvasLayer/Control/HBoxContainer/P2Card/HBoxContainer/Portrait
 var p1instance
 var p2instance 
 
@@ -19,16 +21,20 @@ func _ready():
 	match Globals.player1:
 		Fighters.FOX:
 			p1 = load('res://Characters/Fox/Fox.tscn')
+			p1portrait.texture = preload("res://Characters/Fox/charselect.png")
 		Fighters.MARIO:
 			p1 = load('res://Characters/Mario/Mario.tscn')
+			p1portrait.texture = preload("res://Characters/Mario/Sprites/portrait.png")
 	
 	# Load the correct character for Player 2
 	p2 = load('res://Characters/Mario/Mario.tscn')	
 	match Globals.player2:
 		Fighters.FOX:
 			p2 = load('res://Characters/Fox/Fox.tscn')
+			p2portrait.texture = preload("res://Characters/Fox/portrait.png")
 		Fighters.MARIO:
 			p2 = load('res://Characters/Mario/Mario.tscn')
+			p2portrait.texture = preload("res://Characters/Mario/Sprites/portrait.png")
 
 	# Instantiate and add Player 1 character to the scene
 	p1instance = p1.instantiate()
@@ -38,7 +44,7 @@ func _ready():
 	# Instantiate and add Player 2 character to the scene
 	p2instance = p2.instantiate()
 	add_child(p2instance)  # This should be p2instance, not p1instance
-	p2instance.position = Vector2(384, 64)  # Set position for Player 2
+	p2instance.position = Vector2(-384, 64)  # Set position for Player 2
 
 	# Optional: set a unique ID to distinguish players if needed
 	p1instance.id = 1
@@ -52,7 +58,10 @@ func _ready():
 
 
 func _process(delta):
-	$CanvasLayer/P1Stocks.text = str(p1instance.stocks)
-	$CanvasLayer/P2Stocks.text = str(p2instance.stocks)
+	$CanvasLayer/Control/HBoxContainer/P1Card/HBoxContainer/VBoxContainer/Stocks.text = str(p1instance.stocks)
+	$CanvasLayer/Control/HBoxContainer/P2Card/HBoxContainer/VBoxContainer/Stocks.text = str(p2instance.stocks)
+	$CanvasLayer/Control/HBoxContainer/P1Card/HBoxContainer/VBoxContainer/Percent.text = str(round(p1instance.percentage)) + "%"
+	$CanvasLayer/Control/HBoxContainer/P2Card/HBoxContainer/VBoxContainer/Percent.text = str(round(p2instance.percentage)) + "%"
+	
 	if p1instance.stocks < 1 or p2instance.stocks < 1:
 		get_tree().change_scene_to_file('res://UI/character_select/character_select.tscn')
